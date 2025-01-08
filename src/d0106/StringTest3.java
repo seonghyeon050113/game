b217ea51609b8d0cfbdfc3964c4a26d13c1794a3
Administrator <Administrator@i7E-17>
2025-01-08 12:39:42 +0900

    asdfasf

--- src/d0106/StringTest3.java
+++ src/d0106/StringTest3.java
@@ -4,6 +4,6 @@ public class StringTest3 {
 	public static void main(String[]args) {
 		String str1 = "안녕";
 				System.out.println(str1=="안녕");
-	}
+	} 
 	
 }
--- src/d0108/Lotto3.java
+++ src/d0108/Lotto3.java
@@ -1,5 +1,14 @@
 package d0108;
 
-public class Lotto3 {
+import java.util.Random;
 
+public class Lotto3 {
+	public static void main(String[]args) {
+		Random r = new Random();
+		int[] nums = new int[3];
+		nums[0] = r.nextInt(100);
+		nums[1] = r.nextInt(100);
+		nums[2] = r.nextInt(100);
+		System.out.println(nums[0]);
+	}
 }
